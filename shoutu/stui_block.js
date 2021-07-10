14ef004ce9cec2260bc5fec4751de1226d9be5ed
503609719 <30364506+503609719@users.noreply.github.com>
2021-07-10 11:31:36 +0800

    Update stui_block.js

--- shoutu/stui_block.js
+++ shoutu/stui_block.js
@@ -102,7 +102,7 @@ var stui = {
 			});
 		},
 		'qrcode': function() {
-			$("img.qrcode").attr("src", "https://api.isoyu.com/qr/?m=0&e=L&p=7&url=" + encodeURIComponent(stui.browser.url) + "")
+			$("img.qrcode").attr("src", "https://api.lykep.com/api/qrcode?frame=1&e=L&size=100&text=" + encodeURIComponent(stui.browser.url) + "")
 		}
 	},
 	'common': {//公共基础
